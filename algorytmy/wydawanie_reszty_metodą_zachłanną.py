b499890ca1fed39c7499ee850fe3915c466f2dd7
Neloduka Sobe <neloduka_sobe@protonmail.ch>
2022-09-12 21:42:18 +0200

    Opisano algorytm wydawania reszty metodą zachłanną

--- algorytmy/wydawanie_reszty_metodą_zachłanną.py
+++ algorytmy/wydawanie_reszty_metodą_zachłanną.py
@@ -7,15 +7,21 @@ def wydawanie_reszty(kwota, nominaly):
         kwota - int; kwota reszty do wydania
         nominaly [int]; nominały pieniędzy posortowane malejąco
     """
-    wydane_nominaly = []
+    wydane_nominaly = [] # lista zawierająca wydane nominały
     ind = 0
-    while kwota >= nominaly[-1]:
-        if nominaly[ind] <= kwota:
+
+    while kwota >= nominaly[-1]: # dopóki kwota jest większa od najmniejszego dostępnego nominału
+
+        if nominaly[ind] <= kwota: # Jeśli nominał mieści się w kwocie
+            # Wydaj nominał
             wydane_nominaly.append(nominaly[ind])
             kwota -= nominaly[ind]
-        else:
+
+        else: # W przeciwnym wypadku
+            # Zmień nominał na mniejszy
             ind += 1
-    return wydane_nominaly
+
+    return wydane_nominaly # Zwróć wydane nominały
 
 # Dla nominałów posortowanych malejąco
 def wydawanie_reszty_1(kwota, nominaly):
@@ -23,15 +29,22 @@ def wydawanie_reszty_1(kwota, nominaly):
         kwota - int; kwota reszty do wydania
         nominaly [int]; nominały pieniędzy posortowane rosnąco
     """
-    wydane_nominaly = []
+
+    wydane_nominaly = [] # lista zawierająca wydane nominały
     ind = len(nominaly)-1
-    while kwota >= nominaly[0]:
-        if nominaly[ind] <= kwota:
+
+    while kwota >= nominaly[0]: # dopóki kwota jest większa od najmniejszego dostępnego nominału
+
+        if nominaly[ind] <= kwota: # Jeśli nominał mieści się w kwocie
+            # Wydaj nominał
             wydane_nominaly.append(nominaly[ind])
             kwota -= nominaly[ind]
-        else:
+
+        else: # W przeciwnym wypadku
+            # Zmień nominał na mniejszy
             ind -= 1
-    return wydane_nominaly
+
+    return wydane_nominaly # Zwróć wydane nominały
 
 print(wydawanie_reszty(13.5, [100,50,20,10,5,2,1,0.5]))
 print(wydawanie_reszty_1(13.5, [0.5,1,2,5,10,20,50,100]))
