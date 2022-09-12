1601c777f863a2ec65fd299b6019e2dd9337aa59
Neloduka Sobe <neloduka_sobe@protonmail.ch>
2022-09-12 20:49:42 +0200

    Poprawiono szyfr Cezara

--- algorytmy/szyfr_Cezara.py
+++ algorytmy/szyfr_Cezara.py
@@ -5,19 +5,21 @@
 
 def szyfr(text, n):
     """Funkcja szyfrująca za pomocą szyfru Cezara wiadomości zapisane znakami w alfabecie abcdefghijklmnopqrstuvwxyz"""
-    ret = ''
+    n = n % 26 # Reszta z dzielenia przez długość alfabetu to rzeczywiste przesunięcie liter
+    ret = '' # Wynik działania algorytmu
     for litera in text:
-        if ord(litera) + n > 122:
-            ret += chr(ord(litera) + n - 26)
-        elif ord(litera) + n < 97: # Pętla potrzebna przy rozszyfrowywaniu
-            ret += chr(ord(litera) + n + 26)
-        else:
-            ret += chr(ord(litera) + n)
+        if ord(litera) + n > 122: # Jeżeli nowa liczba przekracza zakres alfabetu z prawej strony
+            ret += chr(ord(litera) + n - 26) # Dodawanie zaszyfrowanego znaku do wyniku
+        elif ord(litera) + n < 97: # Pętla potrzebna przy rozszyfrowywaniu, gdy nowa liczba przekracza zakres alfabetu z lewej strony
+            ret += chr(ord(litera) + n + 26) # Dodawanie zaszyfrowanego znaku do wyniku
+        else: # Gdy nowy kod mieści się w zakresie alfabetu
+            ret += chr(ord(litera) + n) # Dodawanie zaszyfrowanego znaku do wyniku
     return ret
 
 
 def szyfr_2(alfabet, text, n):
     """Funkcja szyfrująca za pomocą szyfru Cezara wiadomości zapisane znakami w podanym alfabecie """
+    n = n % len(alfabet) # Reszta z dzielenia przez długość alfabetu to rzeczywiste przesunięcie liter
     ret = ''
     for litera in text:
         if alfabet.index(litera) + n > len(alfabet):
@@ -28,7 +30,7 @@ def szyfr_2(alfabet, text, n):
     return ret
 
 
-print(szyfr('testz', 4))
-print(szyfr_2('abcdefghijklmnopqrstuvwxyz', 'testz', 4))
-print(szyfr('xiwxd', -4))
-print(szyfr_2('abcdefghijklmnopqrstuvwxyz', 'xiwxd', -4))
+print(szyfr('testz', 400))
+print(szyfr_2('abcdefghijklmnopqrstuvwxyz', 'testz', 400))
+print(szyfr('docdj', -400))
+print(szyfr_2('abcdefghijklmnopqrstuvwxyz', 'docdj', -400))
