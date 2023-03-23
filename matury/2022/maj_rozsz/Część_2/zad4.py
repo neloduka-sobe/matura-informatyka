3e79ce7699c6c6c021d28c4583cc52fd494cd0e5
neloduka-sobe <neloduka_sobe@protonmail.ch>
2023-03-23 22:06:50 +0100

    Dodano podpunkt 4.2 z maja 2022 r.

--- matury/2022/maj_rozsz/Część_2/zad4.py
+++ matury/2022/maj_rozsz/Część_2/zad4.py
@@ -18,3 +18,38 @@ for i in dane:
 
 print("Zad. 4.1")
 print(f"{ile_1=} {pierwsza=}")
+
+def rozklad(x):
+	cp = x
+	ret = []
+	i = 2
+	while i <= int(cp**0.5)+1:
+		while x % i == 0:
+			ret += [i]
+			x //=i
+		i += 1
+	if x > 1:
+		ret += [x]
+	return ret
+
+# 4.2
+print("4.2")
+# a)
+m_a = 0
+i_a = 0
+for i in dane:
+	l = len(rozklad(i))
+	if l > m_a:
+		m_a = l
+		i_a = i
+print(f"a) {m_a} dla {i_a}")
+
+# b)
+m_b = 0
+i_b = 0
+for i in dane:
+	l = len(set(rozklad(i)))
+	if l > m_b:
+		m_b = l
+		i_b = i
+print(f"b) {m_b} dla {i_b}")
