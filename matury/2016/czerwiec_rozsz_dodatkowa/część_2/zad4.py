d24090ab6cf5a280a66e223ba420f35f3e45f995
neloduka-sobe <neloduka_sobe@protonmail.ch>
2023-04-07 11:46:07 +0200

    Dodano zadanie 4 z czerwca 2016 r.

--- matury/2016/czerwiec_rozsz_dodatkowa/część_2/zad4.py
+++ matury/2016/czerwiec_rozsz_dodatkowa/część_2/zad4.py
@@ -29,7 +29,7 @@ for i in dane:
 	if i[1].strip().endswith('a'):
 		d2.setdefault(miejsce, 0)
 		d2[miejsce] += 1
-print("4.2")
+print("\n4.2")
 print(f"miejsce\tilość")
 for i in sorted(d2.keys()):
 	print(f"{i}\t{d2[i]}")
@@ -38,3 +38,47 @@ for i in sorted(d2.keys()):
 kwota_k = 25000
 kwota_m = 30000
 
+sm = 0
+sk = 0
+for i in dane:
+	wiek = 2016 - int(i[2][:4])
+	plec = 'K' if i[1][-1] == 'a' else 'M'
+	if wiek <= 30:
+		if plec == 'K':
+			sk += kwota_k * 0.001
+		else:
+			sm += kwota_m * 0.001
+	elif 31 <= wiek <= 45:
+		if plec == 'K':
+			sk += kwota_k * 0.0015
+		else:
+			sm += kwota_m * 0.0015
+	elif wiek >= 46:
+		if plec == 'K':
+			sk += kwota_k * 0.0012
+		else:
+			sm += kwota_m * 0.0012
+	if wiek > 60:
+		if plec == 'K':
+			sk += 49
+		else:
+			sm += 49
+			
+print("\n4.3")
+print(f"Mężczyźni: {round(sm,2)}; Kobiety: {round(sk,2)}")
+
+
+# 4.4
+d4 = {}
+for i in dane:
+	wiek = 2016 - int(i[2][:4])
+	dekada = str(wiek)[0]
+	d4.setdefault(dekada, 0)
+	d4[dekada] += 1
+
+print("\n4.4 w pliku")
+with open('wykres.csv', 'w') as f:
+	f.write("Przedział wiekowy;Ilosć\n")
+	for i in d4.keys():
+		f.write(f"{i}0-{i}9;{d4[i]}\n")
+	
